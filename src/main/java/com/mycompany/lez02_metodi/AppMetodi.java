51803b936378ba062b481cd7e5a88573ed957c88
tss <tss@debian>
2019-11-07 17:43:54 +0100

    prima versione

--- src/main/java/com/mycompany/lez02_metodi/AppMetodi.java
+++ src/main/java/com/mycompany/lez02_metodi/AppMetodi.java
@@ -17,14 +17,36 @@ public class AppMetodi {
     public static void main(String[] args) {
         // TODO code application logic here
         //chiedere un numero e stamparne il quadrato
-        
+        /*
         int valore1 = Utils.askInt("dimmi la quantita'",1,100);
         float valore2 = Utils.askFloat("dimmi il peso cadauno", 0,20);
         //faccio il quadrato
         float ris =valore1*valore2;
         //faccio stampare ilnuovo valore
-        System.out.println(ris);
-        float sc= Utils.calcolaSconto(99.99f, 12.02f);
+        System.out.println("peso totale="+ris);
+        
+        
+        float prezzo = Utils.askFloat("dimmi il prezzo in euro (es. 50 o 99.99)", 0,2000);
+        float sconto = Utils.askFloat("dimmi lo sconto % (es. 20 o 33.33)", 0,100);
+        
+        float prezzoscontato= Utils.calcolaSconto(prezzo,sconto);
+        String vistx="prezzo =" +prezzo +"\n";
+        vistx += "sconto = " +sconto +"\n";
+        vistx += "prezzo scontato =" +prezzoscontato +"\n";
+        prezzoscontato= Utils.calcolaScorporo(prezzo, sconto);
+        String vistx2="prezzointero =" +prezzo +"\n";
+        vistx2 += "iva = " +sconto +"\n";
+        vistx2 += "prezzo scorporato =" +prezzoscontato +"\n";
+        String rx =Utils.aggiungiACapo(vistx,vistx2);
+        System.out.println(rx);
+*/
+        //caricamento voti
+      int numvoti=Utils.askInt("quanti alunni",1,30);
+        int[] listavoti;
+        listavoti = Utils.caricaVoti(numvoti);
+        String testoTabellone= Utils.creaTabelloneHTML(listavoti);
+        System.out.println(testoTabellone);
+        
         
         
         
--- src/main/java/com/mycompany/lez02_metodi/Utils.java
+++ src/main/java/com/mycompany/lez02_metodi/Utils.java
@@ -99,8 +99,58 @@ public class Utils {
     
     static float calcolaSconto(float prezzo, float sconto){
         float ris=0;
+        //float cento=100;
         ris=prezzo -prezzo*sconto/100;
-        
+        //ris=Math.round(ris*100)/cento;
         return ris;
     }
+    static float calcolaScorporo(float prezzo, float percentuale){
+        float ris=0;
+        //float cento=100;
+        ris= prezzo /(1+percentuale/100);
+        //ris=Math.round(ris*100)/cento;
+        return ris;
+    }
+    static String aggiungiACapo(String testobase,String testoadd){
+        String ris="";
+        ris=testobase  + "\n" + testoadd;
+        return ris; // oppure solo testobase  + "\n" + testoadd;
+    }
+    
+    
+    static int[] caricaVoti(int numvoti){
+        //dichiaro array per i voti grosso numvoti
+        int[] arvoti =new int[numvoti];
+        //ciclo tutti gli elementio dell'array caricasndo il voto con askint
+        for (int i=0;i<numvoti;i++){
+            arvoti[i]=askInt("diimi il voto (0..30)",0,30);
+        }
+        return arvoti;
+        
+    }
+    
+    static String creaTabellone(int[] elencovoti) {
+        String ris = "";
+        float tot = 0;
+        for (int i = 0; i < elencovoti.length; i++) {
+            ris += "#" + (i + 1) + " - " + elencovoti[i] + "\n";
+            tot = tot + elencovoti[i];
+        }
+        float media = tot / elencovoti.length;
+        ris += "voto medio " + media;
+        return ris;
+    }
+    
+    static String creaTabelloneHTML(int[] elencovoti) {
+        String ris = "<table>";
+        float tot = 0;
+        for (int i = 0; i < elencovoti.length; i++) {
+            ris += "<tr><td>#" + (i + 1) + "</td><td><b>" + elencovoti[i] + "</b></td></tr>";
+            tot = tot + elencovoti[i];
+        }
+        float media = tot / elencovoti.length;
+        ris += "</table><br/><b>voto medio " + media;
+        return ris;
+    }
+    
 }
